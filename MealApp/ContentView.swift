8ee5dc8b8a204d73851286dc9e723eb4b353e9da
YoussefAlaaeldin <yousufalaa10@yahoo.com>
2025-05-01 10:07:56 +0300

    Initial Commit

---
+++ MealApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MealApp
+//
+//  Created by Yousuf Abdelfattah on 01/05/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MealApp/MealAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MealAppApp.swift
+//  MealApp
+//
+//  Created by Yousuf Abdelfattah on 01/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct MealAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
